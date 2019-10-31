11c000cb09edd6b8594d98e854c80d220d670d37
Mack Maier <hmaiermack@gmail.com>
2019-10-31 16:23:55 -0700

    finished shopping list drill

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function checkUncheckItem(){
     //watch for click on check button
-    $(".shopping-item-toggle").click(function( event ){
-      $(this).toggleClass("checked");
-      document.getElementsByClassName('checked')[0].style.textDecoration = "line-through";
+    $(".shopping-list").on('click', '.shopping-item-toggle', function( event ){
+      $(this).closest("li").children("span").toggleClass("shopping-item__checked");
     });
-
     //toggle check/unchecked class to object (default should be unchecked)
-    //if check === true add strikethrough to text
 }
 
 function removeItem(){
+  $(".shopping-list").on('click', '.shopping-item-delete', function( event ){
+    $(this).closest("li").remove();
+  });
     //watch for click on delete button
     //remove element when clicked
 }
 
+function newItem(){
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const listItem = $('#shopping-list-entry').val();
+
+    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+  });
+
+
+  }
+newItem();
 checkUncheckItem();
+removeItem();
